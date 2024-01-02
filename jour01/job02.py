5b9202b26c156feefc0f69345d676822f1090bd0
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-01-02 11:48:23 +0100

    ajout job02

--- jour01/job02.py
+++ jour01/job02.py
@@ -1,13 +1,9 @@
 class Operation:
     
-    def __init__(self):
-        self.nombre1 = 12
-        self.nombre2 = 3
-        nombre1 = self.nombre1
-        nombre2 = self.nombre2
+    def __init__(self,nombre1,nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
         print("Le nombre1 est" ,nombre1)
-        print("Le nombre2 est" ,nombre2)
-        
+        print("Le nombre2 est" ,nombre2)        
     
-
-print(Operation())
+print(Operation(12,3))
