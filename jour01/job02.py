d3f5ab16086b2fcf2c6a8deb92565b251893157c
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-01-02 11:45:41 +0100

    ajout job02

---
+++ jour01/job02.py
@@ -0,0 +1,13 @@
+class Operation:
+    
+    def __init__(self):
+        self.nombre1 = 12
+        self.nombre2 = 3
+        nombre1 = self.nombre1
+        nombre2 = self.nombre2
+        print("Le nombre1 est" ,nombre1)
+        print("Le nombre2 est" ,nombre2)
+        
+    
+
+print(Operation())
